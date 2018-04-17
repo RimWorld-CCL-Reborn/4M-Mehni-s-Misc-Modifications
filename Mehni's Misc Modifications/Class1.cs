68b8d81c79e19fe3dceea7dc2ff3383eb2a0880c
Mehni <Mehni@users.noreply.github.com>
2018-04-17 14:53:55 +0200

    Add project files.

---
+++ Mehni's Misc Modifications/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Harmony;
+
+namespace Mehni_s_Misc_Modifications
+{
+    public class Class1
+    {
+    }
+}
