6e1dd58383b9303bbd5ede7df3d0a10de1b11a89
XeoNovaDan <toghill7252010@hotmail.co.uk>
2018-08-16 12:09:33 +0100

    SlothfulWorkSpeedFromTimeTableCodeUpdate:3c:

--- Mehni's Misc Modifications/StatPart_Timetable.cs
+++ Mehni's Misc Modifications/StatPart_Timetable.cs
@@ -19,18 +19,32 @@ namespace Mehni.Misc.Modifications
     public class StatPart_TimeTable : StatPart
     {
 
-        private const float WorkSpeedOffsetNotAssignedToWork = -0.2f;
+        private const float WorkSpeedOffsetLazyAssignment = -0.2f;
+        private const float WorkSpeedOffsetWorkAssignment = 0.2f;
 
         public override void TransformValue(StatRequest req, ref float val)
         {
-            if (MeMiMoSettings.workAssignmentMatters && req.Thing is Pawn pawn && pawn.timetable.CurrentAssignment != TimeAssignmentDefOf.Work)
-                val += WorkSpeedOffsetNotAssignedToWork;
+            if (MeMiMoSettings.workAssignmentMatters && req.Thing is Pawn pawn)
+            {
+                TimeAssignmentDef currentAssignment = pawn.timetable.CurrentAssignment;
+                if (currentAssignment == TimeAssignmentDefOf.Joy || currentAssignment == TimeAssignmentDefOf.Sleep)
+                    val += WorkSpeedOffsetLazyAssignment;
+                if (currentAssignment == TimeAssignmentDefOf.Work)
+                    val += WorkSpeedOffsetWorkAssignment;
+            }
         }
 
         public override string ExplanationPart(StatRequest req)
         {
-            if (MeMiMoSettings.workAssignmentMatters && req.Thing is Pawn pawn && pawn.timetable.CurrentAssignment != TimeAssignmentDefOf.Work)
-                return "M4_StatPart_TimeTable_ExplanationString".Translate() + ": " + WorkSpeedOffsetNotAssignedToWork.ToStringPercent();
+            if (MeMiMoSettings.workAssignmentMatters && req.Thing is Pawn pawn)
+            {
+                TimeAssignmentDef currentAssignment = pawn.timetable.CurrentAssignment;
+                string baseExplanationString = "M4_StatPart_TimeTable_ExplanationString".Translate() + ": ";
+                if (currentAssignment == TimeAssignmentDefOf.Joy || currentAssignment == TimeAssignmentDefOf.Sleep)
+                    return baseExplanationString + WorkSpeedOffsetLazyAssignment.ToStringPercent();
+                if (currentAssignment == TimeAssignmentDefOf.Work)
+                    return baseExplanationString + "+" + WorkSpeedOffsetWorkAssignment.ToStringPercent();
+            }
             return null;
         }
         
